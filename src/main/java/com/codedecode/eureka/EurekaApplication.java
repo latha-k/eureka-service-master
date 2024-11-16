e5cb14585f137f6953fe934d47e4b83cc8763f04
latha-k <lathajava1@gmail.com>
2024-11-16 15:24:37 -0700

    changed main Class1

--- src/main/java/com/codedecode/eureka/EurekaApplication.java
+++ src/main/java/com/codedecode/eureka/EurekaApplication.java
@@ -10,6 +10,10 @@ public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+		System.out.println("within EUREKA SERVICE");
+
+
 	}
+
 
 }
